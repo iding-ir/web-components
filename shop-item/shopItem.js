33593107d372e111dc99d11020ac14e37c2d87a1
Aydin Ghane Kh <aydin.ghane.kh@gmail.com>
2021-12-27 16:21:55 +0330

    Add toggle button to show/hide description

--- shop-item/shopItem.js
+++ shop-item/shopItem.js
@@ -8,8 +8,9 @@ template.innerHTML = `
             grid-template-areas:
                 "image name"
                 "image price"
+                "image toggle"
                 "image description";
-            grid-template-rows: auto auto auto;
+            grid-template-rows: auto auto auto auto;
             grid-template-columns: 200px auto;
             background-color: rgba(130, 130, 130, 0.3)
         }
@@ -36,6 +37,10 @@ template.innerHTML = `
         .description {
             grid-area: description;
         }
+
+        .hidden {
+            display: none;
+        }
     </style>
 
     <div class="item">
@@ -47,6 +52,8 @@ template.innerHTML = `
             <slot name="price" />
         </div>
 
+        <button class="toggle">Hide Description</button>
+
         <p class="description">
             <slot name="description" />
         </p>
@@ -57,6 +64,8 @@ class ShopItem extends HTMLElement {
   constructor() {
     super();
 
+    this.visibility = true;
+
     this.attachShadow({ mode: "open" });
     this.shadowRoot.appendChild(template.content.cloneNode(true));
 
@@ -65,6 +74,30 @@ class ShopItem extends HTMLElement {
 
     this.shadowRoot.querySelector(".image").src = this.getAttribute("image");
     this.shadowRoot.querySelector(".image").alt = this.getAttribute("name");
+  }
+
+  toggle() {
+    this.visibility = !this.visibility;
+
+    if (this.visibility) {
+      this.shadowRoot.querySelector(".toggle").innerText = "Hide Description";
+
+      this.shadowRoot.querySelector(".description").classList.remove("hidden");
+    } else {
+      this.shadowRoot.querySelector(".toggle").innerText = "Show Description";
+
+      this.shadowRoot.querySelector(".description").classList.add("hidden");
+    }
+  }
+
+  connectedCallback() {
+    this.shadowRoot
+      .querySelector(".toggle")
+      .addEventListener("click", () => this.toggle());
+  }
+
+  disconnectedCallback() {
+    this.shadowRoot.querySelector(".toggle").removeEventListener("click");
   }
 }
 
